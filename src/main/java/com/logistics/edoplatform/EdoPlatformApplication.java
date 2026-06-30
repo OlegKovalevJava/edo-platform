a5f8d8c580f22a5282f9b1824361ffa1c57f3a49
OPKovalev <kovalyovop@yandex.ru>
2026-06-30 22:31:49 +0300

    first commit

---
+++ src/main/java/com/logistics/edoplatform/EdoPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.logistics.edoplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdoPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdoPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/logistics/edoplatform/EdoPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistics.edoplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdoPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
